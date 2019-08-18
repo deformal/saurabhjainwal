9e239dc8ae6307deee8d72726ccc3e9779b6dae9
deformal <46109434+deformal@users.noreply.github.com>
2019-08-18 16:27:01 +0530

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,5 +1,54 @@
 
 $(".heading").fadeOut(300).fadeIn(5000);
 $(".heading1").fadeOut(300).fadeIn(5000);
+
+$(document).ready(function() {
+    $(".uparrow").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: -0 }, "slow");
+        return false;
+    });
+});
+
+$(document).ready(function() {
+    $(".abo").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 1080 }, "slow");
+        return false;
+    });2
+});
+
+$(document).ready(function() {
+    $(".ski").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 1900 }, "slow");
+        return false;
+    });2
+});
+$(document).ready(function() {
+    $(".hobs").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 3300 }, "slow");
+        return false;
+    });2
+});
+
+$(document).ready(function() {
+    $(".ga").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 5400 }, "slow");
+        return false;
+    });
+});
+$(document).ready(function() {
+    $(".con").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 7080 }, "slow");
+        return false;
+    });
+});
+
+
+
 AOS.init();
-//$(".skill1").slideUp(300).slideDown(300);
+
